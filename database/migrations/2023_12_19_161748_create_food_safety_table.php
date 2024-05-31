a2d246badfd82476509249d3ae6fdecc72529b8c
karluga <kaarlisbrakis@gmail.com>
2024-05-31 21:35:49 +0300

    changes

--- database/migrations/2023_12_19_161748_create_food_safety_table.php
+++ database/migrations/2023_12_19_161748_create_food_safety_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->integer('safety_id');
             $table->timestamps();
             $table->created_at()->useCurrent();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
     }
 
