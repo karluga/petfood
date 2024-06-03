be19e3f8d8a4055d0ecc0b3bb9d2f612a8c15dbf
karluga <kaarlisbrakis@gmail.com>
2024-06-03 12:43:26 +0300

    fixes

--- resources/views/admin/animals.blade.php
+++ resources/views/admin/animals.blade.php
@@ -5,84 +5,90 @@
     <title>{{ env('APP_NAME', 'Pet Food') }} | Admin Dashboard</title>
 </head>
 <body>
-    <div id="admin-dashboard" class="d-flex">
-        <form action="{{ route('admin.animal.create') }}" method="POST" enctype="multipart/form-data" class="d-flex flex-wrap justify-content-center">
-
-            <div class="white-box mr-2">
-                @csrf
-                <div class="form-group">
-                    <label for="gbif_id" class="mb-1">
-                        <span>GBIF ID from </span>
-                        <a href="https://api.gbif.org/v1/species/search?q=chinchilla">
-                            @svg('assets/icons/gbif-logo.svg', 'gbif-logo')
-                        </a>
-                    </label>
-                    <div class="input-group">
-                        <input type="text" value="{{ isset($data['key']) ? $data['key'] : '' }}" class="form-control" id="gbif_id" name="gbif_id">
-                        <button type="submit" name="autofill" class="btn btn-primary d-flex" id="autofillBtn">Autofill</button>
-                    </div>
-                    @error('gbif_id')
-                        <span class="text-danger">{{ $message }}</span>
-                    @enderror
+<div id="admin-dashboard" class="d-flex">
+    <form action="{{ route('admin.animal.create') }}" method="POST" enctype="multipart/form-data" class="d-flex flex-wrap justify-content-center">
+        <div class="white-box mr-2">
+            @csrf
+            <h1>ADD SPECIES</h1>
+            <div class="form-group required">
+                <label for="gbif_id" class="mb-1">GBIF ID</label>
+                <span>from </span>
+                <a href="https://api.gbif.org/v1/species/search?q=chinchilla">
+                    @svg('assets/icons/gbif-logo.svg', 'gbif-logo')
+                </a>
+                <div class="input-group">
+                    <input type="text" value="{{ old('gbif_id', isset($data['key']) ? $data['key'] : '') }}" class="form-control" id="gbif_id" name="gbif_id">
+                    <button type="submit" name="autofill" class="btn btn-primary d-flex" id="autofillBtn">Autofill</button>
                 </div>
+                @error('gbif_id')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group required">
+                <label for="parent_id" class="mb-1">Parent ID</label>
+                <div class="input-group">
+                    <input type="text" value="{{ old('parent_id', isset($data['parentKey']) ? $data['parentKey'] : '') }}" class="form-control" id="parent_id" name="parent_id">
+                    <button type="submit" name="autofill" class="btn btn-primary d-flex" id="autofillBtn">Autofill</button>
+                </div>
+                @error('parent_id')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group required">
+                <label for="name" class="mb-1">Name</label>
+                <div class="input-group">
+                    <input type="text" value="{{ old('single_name', isset($data['canonicalName']) ? $data['canonicalName'] : '') }}" class="form-control" id="single_name" name="single_name" placeholder="Single">
+                    <input type="text" value="{{ old('plural_name') }}" class="form-control" id="plural_name" name="plural_name" placeholder="Plural">
+                </div>
+            </div>            
+            <div class="form-group required">
+                <label for="language" class="mb-1">Localization</label>
+                <select class="form-control" id="language" name="language">
+                    @foreach($supportedLanguages as $code => $language)
+                        <option value="{{ $code }}" {{ old('language') == $code ? 'selected' : '' }}>{{ $language['name'] }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group required">
+                <label for="category" class="mb-1">Category</label>
+                <select class="form-control" id="category" name="category">
+                    @foreach($categories as $category)
+                        <option value="{{ $category }}" {{ old('category') == $category ? 'selected' : '' }}>{{ $category }}</option>
+                    @endforeach
+                </select>
+            </div>
+            
+            <div class="form-group required">
+                <label for="rank" class="mb-1">Rank</label>
+                <select class="form-control" id="rank" name="rank">
+                    @foreach($ranks as $rank)
+                        <option value="{{ $rank }}" {{ old('rank', isset($data) && $data['rank'] === $rank ? 'selected' : '') }}>{{ $rank }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="appearance" class="mb-1">Appearance</label>
+                <textarea class="form-control" id="appearance" name="appearance" rows="3">{{ old('appearance') }}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="food" class="mb-1">Food</label>
+                <textarea class="form-control" id="food" name="food" rows="3">{{ old('food') }}</textarea>
+            </div>
     
-                <div class="form-group">
-                    <label for="name" class="mb-1">Name</label>
-                    <div class="input-group">
-                        <input type="text" value="{{ isset($data['canonicalName']) ? $data['canonicalName'] : '' }}" class="form-control" id="single_name" name="single_name" placeholder="Single">
-                        <input type="text" class="form-control" id="plural_name" name="plural_name" placeholder="Plural">
-                    </div>
-                </div>            
-                <div class="form-group">
-                    <label for="language" class="mb-1">Localization</label>
-                    <select class="form-control" id="language" name="language">
-                        @foreach($supportedLanguages as $code => $language)
-                            <option value="{{ $code }}">{{ $language['name'] }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="category" class="mb-1">Category</label>
-                    <select class="form-control" id="category" name="category">
-                        @foreach($categories as $category)
-                            <option value="{{ $category }}">{{ $category }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                
-                <div class="form-group">
-                    <label for="rank" class="mb-1">Rank</label>
-                    <select class="form-control" id="rank" name="rank">
-                        @foreach($ranks as $rank)
-                            <option value="{{ $rank }}" {{ isset($data) && $data['rank'] === $rank ? 'selected' : '' }}>{{ $rank }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="appearance" class="mb-1">Appearance</label>
-                    <textarea class="form-control" id="appearance" name="appearance" rows="3"></textarea>
-                </div>
-                <div class="form-group">
-                    <label for="food" class="mb-1">Food</label>
-                    <textarea class="form-control" id="food" name="food" rows="3"></textarea>
-                </div>
-
-                <!-- Image upload -->
-                <div class="form-group">
-                    <label for="images" class="mb-1">Images</label>
-                    <input type="file" class="form-control" id="images" name="images[]" multiple>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+            <!-- Image upload -->
+            <div class="form-group">
+                <label for="images" class="mb-1">Images</label>
+                <input type="file" class="form-control" id="images" name="images[]" multiple accept="image/*">
             </div>
-            <!-- Image preview -->
-            <div class="white-box" id="preview">
-                <span class="mb-1">Preview</span>
-                <div class="image-preview" class="d-flex flex-wrap"></div>
-            </div>
-        </form>
-
-    </div>
-<!-- Image preview script -->
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+        <!-- Image preview -->
+        <div class="white-box" id="preview">
+            <span class="mb-1">Preview</span>
+            <div class="image-preview" class="d-flex flex-wrap"></div>
+        </div>
+    </form>        
+</div>
 <script>
 // JavaScript logic for handling image preview and removal
 const imgInp = document.getElementById('images');
@@ -95,8 +101,9 @@ imgInp.onchange = evt => {
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
         if (!file.type.startsWith('image/')) {
-            alert('Please select only image files.'); // Alert when a non-image file is selected
-            continue; // Skip non-image files
+             // Alert when a non-image file is selected
+            alert('Please select only image files.');
+            return;
         }
         const reader = new FileReader();
         reader.onload = () => {
@@ -161,24 +168,26 @@ imgInp.onchange = evt => {
     }
 };
 
-    </script>
-    
-    <!-- Style for selected image -->
-    <style>
-        .img-thumbnail {
-            border: 2px solid transparent; /* Add border to all images */
-            height: 250px;
-            max-width: 400px;
-        }
-    
-        input[type="radio"]:checked + label .img-thumbnail {
-            border-color: blue; /* Style selected image with border color */
-        }
-    
-        #preview {
-            display: none;
-        }
-    </style>
+</script>
+<style>
+    .img-thumbnail {
+        border: 2px solid transparent; /* Add border to all images */
+        height: 250px;
+        max-width: 400px;
+    }
+
+    input[type="radio"]:checked + label .img-thumbnail {
+        border-color: blue; /* Style selected image with border color */
+    }
+
+    #preview {
+        display: none;
+    }
+    .form-group.required label:after { 
+        color: red;
+        content: "*";
+    }
+</style>
     
 </body>
 @endsection
