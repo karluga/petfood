76f11ceec014a3a1d4875ab896c416a0084e38eb
karluga <kaarlisbrakis@gmail.com>
2024-06-17 02:06:00 +0300

    revert

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -42,7 +42,7 @@ class AdminController extends Controller
                 return back()->withErrors(['gbif_id' => 'Invalid response from GBIF API.']);
             }
     
-            if (isset($data['kingdom']) && $data['kingdom'] !== 'Animalia' && $data['kingdom'] !== 'Metazoa') {
+            if (isset($data['kingdom']) && $data['kingdom'] !== 'Animalia') {
                 return back()->withErrors(['gbif_id' => 'The species has to be an animal']);
             }
     
@@ -75,52 +75,57 @@ class AdminController extends Controller
             'language' => 'required|string|size:2',
             'parent_id' => 'nullable|string',
             'slug' => 'required|string',
-            'images.*' => 'image|mimes:jpeg,png,jpg|max:10240' // 10MB
+            'images.*' => 'image|mimes:jpeg,png,jpg|max:2048'
         ]);
 
         // Handle image upload
         $imagePaths = [];
         $coverImageId = null;
-
-        $illegalChars = ['|', '<', '>', ':', '"', '/', '\\', '?', '*'];
-
+        
         try {
             if ($request->hasFile('images')) {
                 foreach ($request->file('images') as $index => $image) {
+                    // Generate filename based on whether single name is provided or not
                     $imageName = $request->single ? $request->single . ($index + 1) : 'animal' . ($index + 1);
-                    
+        
+                    // Check if single name column has value and modify filename accordingly
                     if (!empty($request->single_name)) {
                         $imageName = $request->single_name . ($index + 1);
                     }
-
-                    // Remove illegal characters from filename
-                    $imageName = str_replace($illegalChars, '', $imageName);
-                    
+        
+                    // Append original uploaded image extension
                     $imageName .= '.' . $image->getClientOriginalExtension();
+        
+                    // Move image to appropriate directory
                     $imagePath = 'assets/images/' . $request->gbif_id;
                     $image->move(public_path($imagePath), $imageName);
+        
+                    // Store image path in array
                     $imagePaths[] = $imagePath . '/' . $imageName;
                 }
             }
         } catch (\Exception $e) {
             return back()->with('error', 'Error uploading image: ' . $e->getMessage());
         }
-
+        
+        // Create animal pictures records
         try {
             foreach ($imagePaths as $imagePath) {
+                // Extract filename from image path
                 $filename = basename($imagePath);
                 $insertedId = \DB::table('animal_pictures')->insertGetId([
                     'gbif_id' => $request->gbif_id,
                     'filename' => $filename
                 ]);
                 if ($request->has('cover_image') && $request->cover_image == $index) {
-                    $coverImageId = $insertedId;
+                    $coverImageId = $insertedId; // Store the cover image ID
                 }
             }
         } catch (\Exception $e) {
             return back()->with('error', 'Error inserting image into database: ' . $e->getMessage());
         }
-
+        
+        // Create animal record
         try {
             $pet = new Animal();
             $pet->gbif_id = $request->gbif_id;
@@ -133,13 +138,14 @@ class AdminController extends Controller
             $pet->language = $request->language;
             $pet->parent_id = $request->parent_id;
             $pet->slug = $request->slug;
-            $pet->cover_image_id = $coverImageId;
+            $pet->cover_image_id = $coverImageId; // Assign cover image ID
             $pet->save();
         } catch (\Exception $e) {
             return back()->with('error', 'Error saving pet record: ' . $e->getMessage());
         }
-
-        return redirect()->route('admin.animal.index')->with('success', 'Pet added successfully.');
+        
+        // Redirect back to the dashboard with success message
+        return redirect()->route('admin.animal.index')->with('success', 'Pet added successfully.'); 
     }
 
     /**
@@ -182,7 +188,6 @@ class AdminController extends Controller
             'gbif_id' => 'required|string',
             'safety_id' => 'required',
             'food_id' => 'required',
-            'description' => 'nullable|max:1000',
         ]);
     
         try {
